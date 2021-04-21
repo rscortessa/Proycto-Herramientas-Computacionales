01fad9316a5493549f2ff6703c1fe4202b2d01ef
Ronalito <rscortessa@unal.edu.co>
2021-04-21 14:13:06 -0500

    El programa retorna el promedio de los valores del tiempo real, el tiempo del proceso y los mflops

--- Proyecto-intermedio/Multiplicaciones/constantes.h
+++ Proyecto-intermedio/Multiplicaciones/constantes.h
@@ -1,14 +1,26 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "papi.h"
+#include <cmath>
+#include<iostream>
 //Funciones de multipliación
 void multiplicacion_directa(const double * A, const double * B, double* C,const int N);
+//Calcular desviación estandar e imprimir
+double desviacion_estandar(double * MFLOPS,double mu);
+void print(double *A,double mu);
 //Variables
  float real_time, proc_time,mflops;
  long long flpops;
  float ireal_time, iproc_time, imflops;
  long long iflpops;
  int retval;
+int cuentas=100;
+double MFLOPS[100]={0};
+double mu_MFLOPS=0;
+double REAL_TIME[100]={0};
+double mu_REAL_TIME=0;
+double PROC_TIME[100]={0};
+double mu_PROC_TIME=0;
  double A[4]={1,0,0,1};
  double B[4]={1,1,1,1};
  double C[4]={0};
